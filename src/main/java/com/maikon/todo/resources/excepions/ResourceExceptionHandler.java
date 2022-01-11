9be42d951c61c7d58b79b7991a7ddfde28ec7278
fariasmk <fariasmk@hotmail.com>
2022-01-11 19:30:45 -0300

    Backend finalizado

---
+++ src/main/java/com/maikon/todo/resources/excepions/ResourceExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.maikon.todo.resources.excepions;
+
+import javax.servlet.ServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.maikon.todo.services.exceptions.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, ServletRequest request) {
+		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(),
+				e.getMessage());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+
+	}
+
+}
---
+++ src/main/java/com/maikon/todo/services/DBService.java
@@ -0,0 +1,29 @@
+package com.maikon.todo.services;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.maikon.todo.domain.Todo;
+import com.maikon.todo.repositories.TodoRepository;
+
+@Service
+public class DBService {
+
+	@Autowired
+	private TodoRepository todoRepository;
+
+	public void instanciaBaseDeDados() {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+
+		Todo t1 = new Todo(null, "Estudar", "Estudar Spring Boot 2 e Angular 11", LocalDateTime.parse("25/03/2022 10:40", formatter), false);
+		Todo t2 = new Todo(null, "Ler", "Ler livro de desenvolvimento pessoal", LocalDateTime.parse("22/03/2021 13:00", formatter), true);
+		Todo t3 = new Todo(null, "Exercícios", "Praticar exercícios físicos", LocalDateTime.parse("21/03/2022 10:15", formatter), false);
+		Todo t4 = new Todo(null, "Meditar", "Meditar durante 30 minutos pela manhã", LocalDateTime.parse("27/03/2021 05:40", formatter), true);
+
+		todoRepository.saveAll(Arrays.asList(t1, t2, t3, t4));
+	}
+}
